59f1dad2b9e14ea6adc278ba1855b8cb2e883625
Denis Pauk <pauk.denis@gmail.com>
2014-03-23 10:17:36 +0200

    add configs
    Change-Id: Iec62874dc4cec9be94d8f35eedc17f431786b90d

--- device_z4u.mk
+++ device_z4u.mk
@@ -13,6 +13,20 @@ TARGET_KERNEL_RECOVERY_CONFIG := z4u_defconfig
 
 $(call inherit-product, build/target/product/full.mk)
 
+# vold config
+PRODUCT_COPY_FILES += \
+    device/htc/z4u/configs/vold.fstab:system/etc/vold.fstab
+
+# Boot ramdisk setup
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.target.rc \
+    remount.qcom
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/htc/z4u/rootdir/etc/fstab.qcom:recovery/root/fstab.qcom
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_z4u
 PRODUCT_DEVICE := z4u
