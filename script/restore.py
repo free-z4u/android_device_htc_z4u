cc11b3f3eb04c0448cdb1f5e39e01e011aa29f9c
Denis Pauk <pauk.denis@gmail.com>
2015-04-04 14:33:32 +0300

    add logic for copy comments from file start

--- script/restore.py
+++ script/restore.py
@@ -23,36 +23,96 @@ def removeCCppComment(text):
 
     return re.sub(pattern, replacer, text)
 
-def cleaupWhiteSpace(text):
-    text = re.sub(pattern_white_space, '\n', text)
-    return text.strip()
+def get_hash(text, strip):
+    cleaned_up_text = removeCCppComment(text)
+    # drop whitespaces
+    cleaned_up_text = re.sub(pattern_white_space, '\n', cleaned_up_text)
+    if strip:
+        # for compare case
+        return cleaned_up_text.strip()
+    else:
+        # for search patern case
+        return cleaned_up_text
+
+
+def search_part_with_same_hash(full, hash):
+    stop = len(hash);
+    # search start of magic
+    start_hash = get_hash(full[:stop], False)
+    while start_hash.strip() != hash:
+        if len(full) <= stop:
+            return None
+        if len(hash) > len(start_hash):
+            # try skip difference between hashes
+            stop = stop + len(hash) - len(start_hash)
+        else:
+            # looks like not hashable
+            stop = stop + 1
+        start_hash = get_hash(full[:stop], False)
+    return full[:stop]
+
+def optimize(src_file, dst_file):
+    src_desc = open(src_file, 'rb')
+    with src_desc:
+        src_text = src_desc.read()
+    if not src_text:
+        return
+    dst_desc = open(dst_file, 'rb')
+    with dst_desc:
+        dst_text = dst_desc.read()
+    if not dst_text:
+        return
+    src_hash = get_hash(src_text, True)
+    dst_hash = get_hash(dst_text, True)
+    common_hash = 0;
+    for i in range(min(len(src_hash), len(dst_hash))):
+        if src_hash[i] != dst_hash[i]:
+           common_hash = src_hash[:i-1]
+           break
+    if common_hash:
+        common_hash = common_hash.strip()
+    if common_hash:
+        print "diff %s => %s %d%%" % (src_file, dst_file, 100 * len(common_hash) / len(dst_hash))
+        common_hash_src = search_part_with_same_hash(src_text, common_hash)
+        if not common_hash_src:
+            print "Check file please %s" % src_file
+            sys.exit()
+        common_hash_dst = search_part_with_same_hash(dst_text, common_hash)
+        if not common_hash_dst:
+            print "Check file please %s" % dst_file
+            sys.exit()
+        if len(common_hash_dst) < len(common_hash_src):
+            src_desc = open(src_file, 'wb')
+            with(src_desc):
+                src_desc.write(common_hash_src + dst_text[len(common_hash_dst):])
+    else:
+        print "diff %s => %s" % (src_file, dst_file)
 
 def file_hash(name):
-    file_desc = open(name, 'r')
+    file_desc = open(name, 'rb')
     with file_desc:
-        text = removeCCppComment(file_desc.read())
-        return cleaupWhiteSpace(text)
+        return get_hash(file_desc.read(), True)
 
 def process(to_dir, from_dir):
     for dirname, _, filenames in os.walk(from_dir):
         # print path to all filenames.
         for filename in filenames:
             if filename.endswith('.c') or filename.endswith('.h'):
-                full_file = os.path.join(dirname, filename)
-                origin_file = full_file.replace(from_dir, to_dir)
-                if os.access(origin_file, os.R_OK):
-                    statinfo_origin = os.stat(origin_file)
-                    statinfo_new = os.stat(full_file)
+                dst_file = os.path.join(dirname, filename)
+                src_file = dst_file.replace(from_dir, to_dir)
+                if os.access(src_file, os.R_OK):
+                    statinfo_origin = os.stat(src_file)
+                    statinfo_new = os.stat(dst_file)
                     if statinfo_new.st_size < statinfo_origin.st_size:
-                        origin_file_hash = file_hash(origin_file)
-                        new_file_hash = file_hash(full_file)
-                        if new_file_hash == origin_file_hash:
-                            shutil.copy2(origin_file, full_file)
-                            print "copy %s => %s" % (origin_file, full_file)
+                        src_file_hash = file_hash(src_file)
+                        new_file_hash = file_hash(dst_file)
+                        if new_file_hash == src_file_hash:
+                            shutil.copy2(src_file, dst_file)
+                            print "copy %s => %s" % (src_file, dst_file)
                         else:
-                            print "diff %s => %s" % (origin_file, full_file)
+                            optimize(src_file, dst_file)
                     else:
-                        print "same %s => %s" % (origin_file, full_file)
+                        print "same %s => %s" % (src_file, dst_file)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
