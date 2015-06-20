4981749a0e4b9aa67cb4ad2892928a59cf7c3e4c
Denis Pauk <pauk.denis@gmail.com>
2015-06-20 14:22:26 +0300

    cm-10.2 - unbootable
    Change-Id: Ie703f21601004b2121b2b3741f9ba926a7fbc551

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := z4u
 
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=/dev/tty0 msm_fb.align_buffer=N ats=1l
 BOARD_KERNEL_BASE := 0x03b00000
 BOARD_KERNEL_PAGESIZE := 2048
 
@@ -35,3 +35,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_PREBUILT_KERNEL := device/htc/z4u/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
